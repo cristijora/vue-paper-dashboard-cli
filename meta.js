add0cf3a1e0028efe57fe0f9437dfa0b152e8a59
cristian.jora <cristian.jora@corebuild.eu>
2017-09-12 14:22:41 +0300

    Remove vue-router installation option

--- meta.js
+++ meta.js
@@ -40,10 +40,6 @@ module.exports = {
         }
       ]
     },
-    "router": {
-      "type": "confirm",
-      "message": "Install vue-router?"
-    },
     "lint": {
       "type": "confirm",
       "message": "Use ESLint to lint your code?"
